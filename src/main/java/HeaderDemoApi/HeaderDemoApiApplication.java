4ce3ed1293cda0f99d3a86dfcdc4f2b808f328d9
AndrewBell <andrew@recursivechaos.com>
2015-04-28 21:12:18 -0500

    Obligatory Hello World

---
+++ src/main/java/HeaderDemoApi/HeaderDemoApiApplication.java
@@ -0,0 +1,12 @@
+package HeaderDemoApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeaderDemoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeaderDemoApiApplication.class, args);
+    }
+}
