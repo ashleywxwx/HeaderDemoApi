6bcc613db2212096ef64cf1facda6fd32802abc6
AndrewBell <andrew@recursivechaos.com>
2015-07-09 22:25:08 -0500

    Send custom OPTIONS response header demo.

--- src/main/java/HeaderDemoApi/config/SimpleCORSFilter.java
+++ src/main/java/HeaderDemoApi/config/SimpleCORSFilter.java
@@ -1,5 +1,7 @@
 package HeaderDemoApi.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
@@ -9,18 +11,24 @@ import java.io.IOException;
 @Component
 public class SimpleCORSFilter implements Filter {
 
+    private final Logger log = LoggerFactory.getLogger(SimpleCORSFilter.class);
+
+
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) res;
+        log.info("request origin: {}" + req.getRemoteHost());
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
         response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, Request-Header, Response-Header");
-        response.setHeader("Access-Control-Expose-Headers", "Response-Header");
+        response.setHeader("Access-Control-Allow-Headers", "HPP-TOKEN, My-Response-Header");
+        response.setHeader("My-Response-Header", "My-Response-Value");
         chain.doFilter(req, res);
     }
 
-    public void init(FilterConfig filterConfig) {}
+    public void init(FilterConfig filterConfig) {
+    }
 
-    public void destroy() {}
+    public void destroy() {
+    }
 
 }
--- src/main/java/HeaderDemoApi/controller/GreetingController.java
+++ src/main/java/HeaderDemoApi/controller/GreetingController.java
@@ -1,31 +1,21 @@
 package HeaderDemoApi.controller;
 
-import HeaderDemoApi.domain.Greeting;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.servlet.http.HttpServletResponse;
-import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
-
     @RequestMapping("/greeting")
-    public Greeting greeting(
-            @RequestParam(value = "name", defaultValue = "World") String name,
-            HttpServletResponse response,
-            @RequestHeader("Request-Header") String headerName) {
+    public HttpEntity<String> getRequest(@RequestHeader(value = "My-Response-Header", defaultValue = "No header found") String token) {
 
-        // Sets our custom response header
-        response.setHeader("Response-Header","Steve");
+        ResponseEntity<String> response = new ResponseEntity<>(token, HttpStatus.OK);
 
-        return new Greeting(counter.incrementAndGet(),
-                String.format(template, headerName));
+        return response;
     }
 
 }
